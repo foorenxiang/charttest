c2065f0b53fa69f41f77ad98ee7a8781e742c4b9
foorenxiang <foorenxiang@gmail.com>
2020-07-16 11:54:40 +0800

    initial commit

---
+++ src/StreamgraphComponent.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
+import vsBenchmarkStats from './vsBenchmark';
+
+export default function StreamgraphComponent(props) {
+  const svgID = (Math.floor(Math.random() * 1000000) + 1000).toString(16) + '_d3graph';
+  useEffect(() => renderChart(svgID, { ...props }));
+
+  return (
+    <>
+      <svg id={svgID} />
+    </>
+  );
+}
+
+const renderChart = (svgID, props) => {
+  // will refactor with prop types
+  const {
+    graphFillColor = '#f38200',
+    xAxisFeature = 'Time',
+    yAxisFeature = 'VSBenchmark',
+    data = vsBenchmarkStats,
+    width = 320,
+    height = 500,
+  } = { ...props };
+
+  const series = d3
+    .stack()
+    .keys(Object.keys(data[0]))
+    .offset(d3.stackOffsetWiggle)
+    .order(d3.stackOrderInsideOut)(data);
+
+  const area = d3
+    .area()
+    .x((d) => x(d.data[xAxisFeature]))
+    .y0((d) => y(d[0]))
+    .y1((d) => y(d[1]));
+
+  const margin = { top: 0, right: 20, bottom: 30, left: 20 };
+
+  // .append("title")
+  // .text(({key}) => key);
+
+  // svg.append('g');
+  // .call(xAxis);
+
+  // const xAxis = (g) =>
+  //   g
+  //     .attr('transform', `translate(0,${height - margin.bottom})`)
+  //     .call(
+  //       d3
+  //         .axisBottom(x)
+  //         .ticks(width / 80)
+  //         .tickSizeOuter(0)
+  //     )
+  //     .call((g) => g.select('.domain').remove());
+
+  const minXScale = data.reduce(
+    (minXScale, sample) => (minXScale < sample[xAxisFeature] ? minXScale : sample[xAxisFeature]),
+    Infinity
+  );
+
+  const maxXScale = data.reduce(
+    (maxXScale, sample) => (maxXScale > sample[xAxisFeature] ? maxXScale : sample[xAxisFeature]),
+    -Infinity
+  );
+
+  const x = d3
+    .scaleLinear()
+    .domain([minXScale, maxXScale])
+    .range([margin.left, width - margin.right]);
+
+  const minDataVal = data.reduce(
+    (minDataVal, sample) => (minDataVal < sample[yAxisFeature] ? minDataVal : sample[yAxisFeature]),
+    Infinity
+  );
+
+  const maxDataVal = data.reduce(
+    (maxDataVal, sample) => (maxDataVal > sample[yAxisFeature] ? minDataVal : sample[yAxisFeature]),
+    -Infinity
+  );
+
+  const y = d3
+    .scaleLinear()
+    .domain([minDataVal, maxDataVal])
+    .range([height - margin.bottom, margin.top]);
+
+  const color = d3
+    .scaleOrdinal()
+    .domain(Object.keys(data[0]))
+    .range(d3.schemeCategory10)
+    // .range(['#f38200', '#50c369']);
+    .range(['#fff', graphFillColor]);
+
+  const svg = d3.select(svgID).create('svg').attr('viewBox', [0, 0, width, height]);
+
+  svg
+    .append('g')
+    .selectAll('path')
+    .data(series)
+    .join('path')
+    .attr('fill', ({ key }) => color(key))
+    .attr('d', area);
+
+  return svg;
+};
---
+++ src/vsBenchmark.js
@@ -0,0 +1,546 @@
+export default [
+  {
+    Time: 1880,
+    VSBenchmark: 241,
+  },
+  {
+    Time: 1881,
+    VSBenchmark: 263,
+  },
+  {
+    Time: 1882,
+    VSBenchmark: 288,
+  },
+  {
+    Time: 1883,
+    VSBenchmark: 287,
+  },
+  {
+    Time: 1884,
+    VSBenchmark: 337,
+  },
+  {
+    Time: 1885,
+    VSBenchmark: 339,
+  },
+  {
+    Time: 1886,
+    VSBenchmark: 370,
+  },
+  {
+    Time: 1887,
+    VSBenchmark: 338,
+  },
+  {
+    Time: 1888,
+    VSBenchmark: 404,
+  },
+  {
+    Time: 1889,
+    VSBenchmark: 413,
+  },
+  {
+    Time: 1890,
+    VSBenchmark: 392,
+  },
+  {
+    Time: 1891,
+    VSBenchmark: 371,
+  },
+  {
+    Time: 1892,
+    VSBenchmark: 455,
+  },
+  {
+    Time: 1893,
+    VSBenchmark: 387,
+  },
+  {
+    Time: 1894,
+    VSBenchmark: 418,
+  },
+  {
+    Time: 1895,
+    VSBenchmark: 431,
+  },
+  {
+    Time: 1896,
+    VSBenchmark: 367,
+  },
+  {
+    Time: 1897,
+    VSBenchmark: 354,
+  },
+  {
+    Time: 1898,
+    VSBenchmark: 371,
+  },
+  {
+    Time: 1899,
+    VSBenchmark: 326,
+  },
+  {
+    Time: 1900,
+    VSBenchmark: 377,
+  },
+  {
+    Time: 1901,
+    VSBenchmark: 317,
+  },
+  {
+    Time: 1902,
+    VSBenchmark: 301,
+  },
+  {
+    Time: 1903,
+    VSBenchmark: 247,
+  },
+  {
+    Time: 1904,
+    VSBenchmark: 294,
+  },
+  {
+    Time: 1905,
+    VSBenchmark: 311,
+  },
+  {
+    Time: 1906,
+    VSBenchmark: 260,
+  },
+  {
+    Time: 1907,
+    VSBenchmark: 285,
+  },
+  {
+    Time: 1908,
+    VSBenchmark: 260,
+  },
+  {
+    Time: 1909,
+    VSBenchmark: 271,
+  },
+  {
+    Time: 1910,
+    VSBenchmark: 278,
+  },
+  {
+    Time: 1911,
+    VSBenchmark: 280,
+  },
+  {
+    Time: 1912,
+    VSBenchmark: 310,
+  },
+  {
+    Time: 1913,
+    VSBenchmark: 346,
+  },
+  {
+    Time: 1914,
+    VSBenchmark: 375,
+  },
+  {
+    Time: 1915,
+    VSBenchmark: 412,
+  },
+  {
+    Time: 1916,
+    VSBenchmark: 421,
+  },
+  {
+    Time: 1917,
+    VSBenchmark: 445,
+  },
+  {
+    Time: 1918,
+    VSBenchmark: 397,
+  },
+  {
+    Time: 1919,
+    VSBenchmark: 379,
+  },
+  {
+    Time: 1920,
+    VSBenchmark: 379,
+  },
+  {
+    Time: 1921,
+    VSBenchmark: 392,
+  },
+  {
+    Time: 1922,
+    VSBenchmark: 341,
+  },
+  {
+    Time: 1923,
+    VSBenchmark: 362,
+  },
+  {
+    Time: 1924,
+    VSBenchmark: 341,
+  },
+  {
+    Time: 1925,
+    VSBenchmark: 310,
+  },
+  {
+    Time: 1926,
+    VSBenchmark: 310,
+  },
+  {
+    Time: 1927,
+    VSBenchmark: 234,
+  },
+  {
+    Time: 1928,
+    VSBenchmark: 247,
+  },
+  {
+    Time: 1929,
+    VSBenchmark: 209,
+  },
+  {
+    Time: 1930,
+    VSBenchmark: 196,
+  },
+  {
+    Time: 1931,
+    VSBenchmark: 209,
+  },
+  {
+    Time: 1932,
+    VSBenchmark: 213,
+  },
+  {
+    Time: 1933,
+    VSBenchmark: 199,
+  },
+  {
+    Time: 1934,
+    VSBenchmark: 189,
+  },
+  {
+    Time: 1935,
+    VSBenchmark: 211,
+  },
+  {
+    Time: 1936,
+    VSBenchmark: 192,
+  },
+  {
+    Time: 1937,
+    VSBenchmark: 176,
+  },
+  {
+    Time: 1938,
+    VSBenchmark: 192,
+  },
+  {
+    Time: 1939,
+    VSBenchmark: 185,
+  },
+  {
+    Time: 1940,
+    VSBenchmark: 218,
+  },
+  {
+    Time: 1941,
+    VSBenchmark: 223,
+  },
+  {
+    Time: 1942,
+    VSBenchmark: 295,
+  },
+  {
+    Time: 1943,
+    VSBenchmark: 284,
+  },
+  {
+    Time: 1944,
+    VSBenchmark: 238,
+  },
+  {
+    Time: 1945,
+    VSBenchmark: 280,
+  },
+  {
+    Time: 1946,
+    VSBenchmark: 274,
+  },
+  {
+    Time: 1947,
+    VSBenchmark: 310,
+  },
+  {
+    Time: 1948,
+    VSBenchmark: 306,
+  },
+  {
+    Time: 1949,
+    VSBenchmark: 333,
+  },
+  {
+    Time: 1950,
+    VSBenchmark: 379,
+  },
+  {
+    Time: 1951,
+    VSBenchmark: 409,
+  },
+  {
+    Time: 1952,
+    VSBenchmark: 418,
+  },
+  {
+    Time: 1953,
+    VSBenchmark: 428,
+  },
+  {
+    Time: 1954,
+    VSBenchmark: 428,
+  },
+  {
+    Time: 1955,
+    VSBenchmark: 452,
+  },
+  {
+    Time: 1956,
+    VSBenchmark: 643,
+  },
+  {
+    Time: 1957,
+    VSBenchmark: 667,
+  },
+  {
+    Time: 1958,
+    VSBenchmark: 796,
+  },
+  {
+    Time: 1959,
+    VSBenchmark: 858,
+  },
+  {
+    Time: 1960,
+    VSBenchmark: 977,
+  },
+  {
+    Time: 1961,
+    VSBenchmark: 1057,
+  },
+  {
+    Time: 1962,
+    VSBenchmark: 948,
+  },
+  {
+    Time: 1963,
+    VSBenchmark: 1035,
+  },
+  {
+    Time: 1964,
+    VSBenchmark: 1275,
+  },
+  {
+    Time: 1965,
+    VSBenchmark: 1650,
+  },
+  {
+    Time: 1966,
+    VSBenchmark: 2329,
+  },
+  {
+    Time: 1967,
+    VSBenchmark: 2663,
+  },
+  {
+    Time: 1968,
+    VSBenchmark: 2430,
+  },
+  {
+    Time: 1969,
+    VSBenchmark: 2817,
+  },
+  {
+    Time: 1970,
+    VSBenchmark: 3550,
+  },
+  {
+    Time: 1971,
+    VSBenchmark: 4133,
+  },
+  {
+    Time: 1972,
+    VSBenchmark: 4181,
+  },
+  {
+    Time: 1973,
+    VSBenchmark: 5627,
+  },
+  {
+    Time: 1974,
+    VSBenchmark: 7476,
+  },
+  {
+    Time: 1975,
+    VSBenchmark: 12653,
+  },
+  {
+    Time: 1976,
+    VSBenchmark: 15591,
+  },
+  {
+    Time: 1977,
+    VSBenchmark: 18280,
+  },
+  {
+    Time: 1978,
+    VSBenchmark: 20522,
+  },
+  {
+    Time: 1979,
+    VSBenchmark: 31927,
+  },
+  {
+    Time: 1980,
+    VSBenchmark: 35819,
+  },
+  {
+    Time: 1981,
+    VSBenchmark: 34373,
+  },
+  {
+    Time: 1982,
+    VSBenchmark: 34210,
+  },
+  {
+    Time: 1983,
+    VSBenchmark: 33754,
+  },
+  {
+    Time: 1984,
+    VSBenchmark: 33906,
+  },
+  {
+    Time: 1985,
+    VSBenchmark: 39050,
+  },
+  {
+    Time: 1986,
+    VSBenchmark: 40522,
+  },
+  {
+    Time: 1987,
+    VSBenchmark: 41786,
+  },
+  {
+    Time: 1988,
+    VSBenchmark: 39451,
+  },
+  {
+    Time: 1989,
+    VSBenchmark: 36827,
+  },
+  {
+    Time: 1990,
+    VSBenchmark: 34405,
+  },
+  {
+    Time: 1991,
+    VSBenchmark: 28887,
+  },
+  {
+    Time: 1992,
+    VSBenchmark: 25034,
+  },
+  {
+    Time: 1993,
+    VSBenchmark: 20809,
+  },
+  {
+    Time: 1994,
+    VSBenchmark: 18715,
+  },
+  {
+    Time: 1995,
+    VSBenchmark: 16345,
+  },
+  {
+    Time: 1996,
+    VSBenchmark: 13973,
+  },
+  {
+    Time: 1997,
+    VSBenchmark: 12239,
+  },
+  {
+    Time: 1998,
+    VSBenchmark: 10908,
+  },
+  {
+    Time: 1999,
+    VSBenchmark: 9741,
+  },
+  {
+    Time: 2000,
+    VSBenchmark: 8550,
+  },
+  {
+    Time: 2001,
+    VSBenchmark: 6963,
+  },
+  {
+    Time: 2002,
+    VSBenchmark: 6132,
+  },
+  {
+    Time: 2003,
+    VSBenchmark: 5339,
+  },
+  {
+    Time: 2004,
+    VSBenchmark: 4677,
+  },
+  {
+    Time: 2005,
+    VSBenchmark: 4088,
+  },
+  {
+    Time: 2006,
+    VSBenchmark: 3355,
+  },
+  {
+    Time: 2007,
+    VSBenchmark: 3038,
+  },
+  {
+    Time: 2008,
+    VSBenchmark: 2439,
+  },
+  {
+    Time: 2009,
+    VSBenchmark: 1952,
+  },
+  {
+    Time: 2010,
+    VSBenchmark: 1655,
+  },
+  {
+    Time: 2011,
+    VSBenchmark: 1409,
+  },
+  {
+    Time: 2012,
+    VSBenchmark: 1228,
+  },
+  {
+    Time: 2013,
+    VSBenchmark: 1064,
+  },
+  {
+    Time: 2014,
+    VSBenchmark: 1048,
+  },
+  {
+    Time: 2015,
+    VSBenchmark: 1013,
+  },
+];
